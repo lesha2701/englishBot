b1e2ab9157d06be41191b805cb75bec4faa85581
Алексей Артемович <81881832+lesha2701@users.noreply.github.com>
2025-03-26 22:16:27 +0300

    create base

---
+++ handlers/start.py
@@ -0,0 +1,10 @@
+from aiogram import Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def start(message: Message) -> None:
+    await message.answer("Hello, AIOgram 3.x!")
